f1800e8b020b9688a277c458fd6183d3ba808fcc
danielkrupinski <danielkrupinski@outlook.com>
2017-12-26 12:34:07 +0100

    Add junk code

---
+++ JunkCode.h
@@ -0,0 +1,623 @@
+#pragma once
+
+	void zPpw() {
+		double qMS = 2.630459E-24;
+		byte BPH = 91;
+		float mlK = -1.90495E+18F;
+		byte WMfM = 10;
+		double uJq = -2.704522E+11;
+		double qDkZ = 2.857202E+20;
+		double Neap = -1.171811E-06;
+		float ULe = -7.044548E+22F;
+		double hIpm = 3.370856E+09;
+		float NTi = -1.088527E+26F;
+		short YRA = 5465;
+		float bxj = 3.127792E-24F;
+		float wbK = -2.855574E+15F;
+		byte Mum = 235;
+		byte NlP = 5;
+		int wtE = 88;
+		if (wtE == 46447) {
+			wtE = 581786;
+		}int iqu = 408478758;
+		if (iqu == 486136) {
+			iqu += 865366;
+		}long long oBm = 28998913022475075;
+		byte qms = 72;
+		byte mRLO = 172;
+		int LsL = 7682;
+		int oNf = 951;
+		float OOR = -0.1036569F;
+
+	}void JFM() {
+		byte YpV = 21;
+		short wEWz = 11986;
+		short RltB = 15912;
+		int bmsm = 9499890;
+		if (bmsm == 661372) {
+			bmsm += 842332;
+		}float CYcM = -6.772588E-35F;
+		float RbHV = 1.726339E+35F;
+		int Yqu = 39;
+		if (Yqu == 210184) {
+			Yqu = 922709;
+		}short fsAX = 26643;
+		int fTUa = 473798834;
+		if (fTUa == 538910) {
+			fTUa = fTUa + 323928;
+		}long long jVjb = 12581158940436472;
+		int UYh = 8186;
+		if (UYh == 152042) {
+			UYh += 88590;
+		}float lOT = 7.139118E+19F;
+		short luuL = -4227;
+		long long oHQ = 68178883222747054;
+		int RQR = 920552;
+		byte XBq = 12;
+		double pyFu = 1.635443E-14;
+		float FAyq = 1.781118E+15F;
+		float CBS = 2.196446E+31F;
+		float agj = -3.180581E+37F;
+		int fKLp = 39914114;
+
+	}void ILa() {
+		long long kqY = 73659218752443558;
+		int lMd = 7241;
+		if (lMd == 7241) {
+			lMd = lMd + 314867;
+		}long long AtsI = 24618673713250560;
+		short TYT = -1650;
+		long long JWeR = 22455709392771857;
+		int BTSo = 951273;
+		short cENA = 24851;
+		float ZwL = 7546.75F;
+		short Phk = 6140;
+		long long Kih = 68803033628190938;
+		long long YMZP = 13104932916092768;
+		double cSnH = 1.005502E-36;
+		byte STl = 144;
+		byte MlT = 253;
+		long long lXsG = 54526444276584377;
+		double iOP = -1.385347E+10;
+		double Jme = 1.960567E+16;
+		short lHwu = 1599;
+
+	}void LMT() {
+		double LppB = -1.05638E-08;
+		float bmK = 6.571107E-32F;
+		int PEh = 76063989;
+		byte jNG = 66;
+		long long HLd = 41633659128947984;
+		byte zmUu = 187;
+		short SfcO = 11934;
+		double Xwcu = -2.164926E-27;
+		short GfH = 23596;
+		double qbx = -0.05331209;
+		int ABk = 692975137;
+		if (ABk == 487743) {
+			ABk = ABk + 491017;
+		}int cKe = 66653211;
+		if (cKe == 539985) {
+			cKe = 633575;
+		}int DIi = 52;
+		if (DIi == 742235) {
+			DIi = 974642;
+		}long long hpZ = 75969217613365868;
+		short YEQ = 2253;
+		double uFf = -5.651301E-31;
+		int CSMk = 19274977;
+		short WLY = -4132;
+		float ajpB = -6.015755E-10F;
+		double Vfx = -2.611415E+32;
+		long long faB = 42557331613200371;
+		byte oAB = 221;
+		int pBW = 7771;
+		if (pBW == 751426) {
+			pBW = 458294;
+		}double xYca = -1.811571E+19;
+
+	}void iJN() {
+		byte flGp = 98;
+		double RxGH = 8.197929E+24;
+		float bGyh = -5.982445E-27F;
+		double HAb = 8.85568E+33;
+		byte gsaK = 5;
+		double VYWc = 8.007809E-08;
+		long long NTNm = 33838090402215440;
+		int TpHg = 991;
+		if (TpHg == 350041) {
+			TpHg = TpHg + 273931;
+		}short UGP = -15637;
+		byte EqO = 17;
+		int TTD = 759671;
+		if (TTD == 420964) {
+			TTD = TTD + 424488;
+		}long long BkQR = 68568421148123507;
+		int Dak = 52193178;
+		if (Dak == 543707) {
+			Dak = Dak + 810972;
+		}int kzq = 98;
+		if (kzq == 920030) {
+			kzq = kzq + 241865;
+		}long long gNMA = 31937503879934954;
+		float cchX = 4.227039E+22F;
+		float gmuI = -4.925123E-26F;
+		int QSu = 7890522;
+		if (QSu == 7890522) {
+			QSu += 815793;
+		}long long eNaa = 69037042325339194;
+		short XkWI = 29587;
+		long long BldR = 20944398777343150;
+		short Gdwq = -7935;
+		double RSV = 1.708794E-13;
+
+	}void yGF() {
+		int bAo = 232659985;
+		if (bAo == 420531) {
+			bAo += 561402;
+		}float cSZm = 662488.1F;
+		byte NSkp = 114;
+		double MzR = -9.461095E-29;
+		double JxhB = -7.942898E-32;
+		int nHA = 344001;
+		long long fLaX = 29789372750484457;
+		short ewT = 18478;
+		float Pzo = 0.008455751F;
+		short qhWA = -1422;
+		double EYnL = 1.543011E+25;
+		long long Kfo = 31086396254484176;
+		int dQFa = 75969440;
+		if (dQFa == 75969440) {
+			dQFa += 990955;
+		}float bZh = -26.92054F;
+		double nlKL = -2.079195E-06;
+		double ZGin = 4.478222E-39;
+		short eKi = 14181;
+		int jzwC = 2844;
+		double lqCp = 6253715;
+		short PmMh = 14017;
+		double EEf = -5.46889E+12;
+		byte ndZ = 103;
+		short QWp = 32763;
+		int gJGm = 817418191;
+		if (gJGm == 27233) {
+			gJGm = 535131;
+		}short hjzD = -3437;
+
+	}void sgDK() {
+		double duAb = -2.288881E-25;
+		byte bWI = 1;
+		double hos = -5.096015E-05;
+		long long gyy = 73986900234480223;
+		long long tVGz = 24102694701895193;
+		long long FqSc = 783185530537882;
+		double YBdo = 9.057195E-14;
+		short Xhe = -20492;
+		int Fab = 8065;
+		double JSB = -4.487727E+28;
+		int GdUz = 983376475;
+		if (GdUz == 139404) {
+			GdUz = 527489;
+		}float LNI = 5.829641E+21F;
+		long long CFzS = 69760125822232811;
+		int Aol = 983871;
+		int MqT = 81484339;
+		if (MqT == 648726) {
+			MqT = 852669;
+		}byte ZPsL = 60;
+		short JZC = 13887;
+		int JZD = 638484;
+		double SDo = 1.978841E-30;
+		long long GHQG = 59669679849656303;
+		int HRNb = 489453543;
+		if (HRNb == 409080) {
+			HRNb = HRNb + 882210;
+		}long long meD = 19931601741022110;
+		double aLh = 5.329471E-16;
+		int MMQu = 642293;
+		if (MMQu == 498224) {
+			MMQu = 389565;
+		}double JnRH = 9.258195E+12;
+		byte npTk = 56;
+		short sXRw = -20494;
+		byte UDpc = 0;
+
+	}void WMqx() {
+		int iwO = 7719;
+		if (iwO == 46236) {
+			iwO = 971636;
+		}int mkhZ = 2622;
+		float eYjF = 2.030744E-35F;
+		double YEqH = 1.103245E-32;
+		long long RDeI = 70962636049572580;
+		double WEi = -0.1562482;
+		short QXlh = 11113;
+		byte bun = 241;
+		byte iiG = 254;
+		short ujEC = 4656;
+		long long nbV = 27142402095683387;
+		double dBD = -0.0005539052;
+		short AJx = -24611;
+		double piyj = -9.856311E-07;
+		byte xdjt = 245;
+		short NiX = -1827;
+		double Qof = 5.892651E-28;
+		byte VsmO = 73;
+		double tQQh = 6.316208E-32;
+		double QQD = 28766.35;
+		short Yiu = -9655;
+		byte iVf = 136;
+		short AUE = -7359;
+		int WZD = 268056;
+		if (WZD == 927819) {
+			WZD = WZD + 422144;
+		}byte xqO = 30;
+		short GhQG = -24909;
+		double eXh = -4.066278E-11;
+		long long LuBh = 26631428651956827;
+		int LKU = 327665041;
+		byte RzJ = 229;
+
+	}void zFn() {
+		long long OCK = 38856799206625326;
+		short sXB = -12585;
+		long long CqRu = 87284865593493915;
+		float zKb = 1.643364E+17F;
+		byte FaO = 135;
+		long long owG = 73875998540165902;
+		float zFt = 1.051372E+26F;
+		byte AKOd = 183;
+		int ETNp = 32;
+		if (ETNp == 733857) {
+			ETNp = ETNp + 325962;
+		}byte Lgx = 142;
+		byte CBKz = 143;
+		byte CScW = 23;
+		short IqiG = 25576;
+		double GseP = -1.447797E+14;
+		double tBkN = -6.566878E-31;
+		int Lig = 80;
+		int owh = 67048552;
+		byte xAXY = 35;
+		double wHm = -6.767837E+13;
+		int JKCq = 4086;
+		if (JKCq == 70992) {
+			JKCq += 345745;
+		}long long LpN = 72214318266180004;
+		byte SWtS = 33;
+
+	}void njt() {
+		byte OWS = 242;
+		double oWa = -2.590057E+26;
+		short KmnL = 1815;
+		long long YcPh = 87389961564253115;
+		float spkV = -2.002865E+31F;
+		short Uhx = 28364;
+		short iPBK = -1006;
+		short eKQT = 21641;
+		float scRM = 8.706598E-36F;
+		float OdJ = -9.599476E+35F;
+		int diwX = 11431539;
+		if (diwX == 76165) {
+			diwX += 125546;
+		}long long OcS = 54432597015174913;
+		float LeW = -1.054453E-10F;
+		int sLH = 710065;
+		int VySU = 753553443;
+		if (VySU == 12318) {
+			VySU = 785797;
+		}long long WYX = 310745087834946;
+		double KFL = -2.279749E+18;
+		float lXq = 3.479108E-23F;
+		byte Veq = 161;
+		long long JtjF = 47444765583966789;
+		int yUiB = 53420771;
+		short xgIf = 26092;
+		double FHcb = 4.148084E-21;
+
+	}void jksB() {
+		double USqU = -0.205814;
+		double Twx = 2.223822E-07;
+		float tKFw = -6.116795E+27F;
+		long long jyHI = 79306518416672306;
+		double bUhT = -4.405583E-16;
+		float dIbU = 1.828062E-31F;
+		byte Smqq = 237;
+		float BaF = 1.592719E-08F;
+		double bGWh = 1.569371E-38;
+		double ZbPp = 1.334168E-17;
+		byte FDoj = 153;
+		float bVD = -1.757796E-29F;
+		int qwJd = 42;
+		if (qwJd == 413605) {
+			qwJd = qwJd + 430073;
+		}double aQz = 6.451355E-09;
+		int NCVa = 30392219;
+		if (NCVa == 865960) {
+			NCVa = NCVa + 62600;
+		}int bOH = 46009;
+		if (bOH == 753946) {
+			bOH = bOH + 375127;
+		}long long iiZZ = 36208857071994312;
+		byte EXEU = 196;
+		long long WDwx = 29611010230366492;
+		short ezQf = 23828;
+		float LRWu = -1.0317E-31F;
+		float wOL = -7.915337E-20F;
+
+	}void SRH() {
+		long long keK = 31609411217637052;
+		float lBSe = -5.161515E+17F;
+		long long XUip = 18900016694953012;
+		double WCQk = 1.754632E-13;
+		float KNm = 1.508304E+26F;
+		short cuw = -13619;
+		float QqV = 8.080054E+21F;
+		float ChH = 3.098159E+20F;
+		byte ZdLb = 39;
+		float Rgm = 8.740361E-37F;
+		float DyI = 9.562336E-20F;
+		double Nca = 5916.846;
+		int LPPO = 267400;
+		float OqS = -1.470364E+26F;
+		short Hxmn = 26346;
+		short LOJR = -23036;
+		byte NqYl = 13;
+		double byV = -1.226083E-06;
+		double ySa = -2.739172E+26;
+		double mfso = -24134.69;
+		long long uWE = 82369647313972371;
+		long long aWEW = 46251009302081155;
+		short aAqU = 8958;
+		short giZT = 31565;
+		float KkcP = -0.007612368F;
+		long long yfT = 13858634167701136;
+		byte cDxg = 92;
+
+	}void xIO() {
+		double IXz = 9.590978E+24;
+		byte kEM = 70;
+		byte ATxF = 19;
+		double npOW = 2.390543;
+		long long RedY = 60774499377821439;
+		double kbwc = 1.675474E+12;
+		byte HSin = 201;
+		byte PQK = 251;
+		long long yGo = 64135998395406422;
+		double Gqm = -1.755953E-10;
+		short bpI = -2055;
+		long long gRlB = 47287344995112513;
+		long long gHlY = 65428659294853416;
+		long long GSpL = 81963776937553036;
+		short GTK = 17344;
+		int GGV = 65279357;
+		int cxwT = 75366477;
+		if (cxwT == 114207) {
+			cxwT += 493328;
+		}byte Fdk = 94;
+		double CZik = -9.021396E-32;
+		long long VbP = 68644302522192404;
+
+	}void mgVE() {
+		int yfB = 912222;
+		double FYf = 2.017368E-20;
+		long long cwE = 36697827836157469;
+		byte Nwji = 187;
+		double BzuJ = 2.039472E+34;
+		short wfGm = 32590;
+		long long nbzh = 66220492503131736;
+		byte JXm = 29;
+		float GODE = 2.540941E+28F;
+		double AjcN = 63.67869;
+		long long Vgmw = 49891861692404152;
+		int ScE = 4977;
+		if (ScE == 245716) {
+			ScE = ScE + 523107;
+		}float Xnf = -2.951634E-18F;
+		int qGbJ = 59918267;
+		long long QHmT = 69340699891004370;
+		double hZe = 5.734181E-06;
+		double qfj = 2.249827E+20;
+		float Ntwi = -2.52333E+20F;
+		byte deK = 165;
+		float eeY = 286.8759F;
+		double Vua = -3.663599E-05;
+		double Gbhb = 2.509341E-31;
+		long long FuzT = 46546962781319049;
+
+	}void iiL() {
+		short bJxH = 26130;
+		double zUg = -6.796466E+28;
+		double VJoY = 3.785557E-35;
+		double WIQ = -1.907923E-31;
+		double ZwkL = -3.903352E-10;
+		float VYDe = 5.497479E+36F;
+		long long fdp = 26650201595999075;
+		double DoK = -1.466093E-17;
+		short PDx = 6355;
+		double HJx = 2.80455E-31;
+		byte Dns = 151;
+		short YxYu = -9028;
+		byte XkkR = 99;
+		double TAY = 7.840427E-12;
+		double qeC = 2.828959E-12;
+		byte fGbN = 138;
+		int WUt = 65;
+		if (WUt == 987466) {
+			WUt = WUt + 971161;
+		}float cFB = -4.185063E-38F;
+		byte kyy = 241;
+		byte tsmZ = 158;
+		float Mtz = -3.385312E-07F;
+		short FulC = 346;
+		float Yee = 1.568585E+22F;
+		byte sgxz = 34;
+
+	}void dUD() {
+		long long dZBy = 16364238628781199;
+		int XbEo = 233489;
+		if (XbEo == 418266) {
+			XbEo += 94878;
+		}double ihU = 1.089882E-32;
+		double UBT = 9.968962E-15;
+		short kIs = 28969;
+		long long kGe = 11930107681220429;
+		short wYk = 19457;
+		float Rmf = -2.173776E-25F;
+		int fjA = 28;
+		if (fjA == 735248) {
+			fjA = fjA + 236733;
+		}float osND = 2.367272E-33F;
+		short NDW = 15847;
+		float ZKJB = 2.155759E-25F;
+		float HFbg = -6.128037E-22F;
+		byte Nklz = 104;
+		byte JZAw = 75;
+		byte npfI = 57;
+		byte ilH = 46;
+		double Ogd = 2.844737E-31;
+		float ECXi = -4.609977E-26F;
+		short utqW = -22789;
+		double HmNB = -0.0004334874;
+		short eMU = -23789;
+		int Tzq = 39406;
+		byte fnUH = 43;
+		int eHlY = 666984;
+		if (eHlY == 219695) {
+			eHlY = eHlY + 803349;
+		}float WpCs = 1.636241E+33F;
+
+	}void ecS() {
+		float KlM = 6.211439E-06F;
+		long long Wzq = 71108478393377679;
+		int ACh = 570632;
+		if (ACh == 692363) {
+			ACh += 853779;
+		}short lje = -15449;
+		int ilWs = 488511;
+		if (ilWs == 913768) {
+			ilWs += 110070;
+		}short CkN = -27938;
+		long long QxiI = 64913281367124922;
+		short MGtj = 15936;
+		float Xme = 5.585471E+10F;
+		double nHf = 1.724932E+27;
+		long long hPeL = 86259285349001840;
+		short fKh = 9510;
+		int ufh = 7467;
+		if (ufh == 903936) {
+			ufh = ufh + 878888;
+		}long long qQw = 1426200540614567;
+		int ajcH = 973092627;
+		short pqk = -3982;
+		int RtdW = 701598;
+		long long sit = 79815900029882033;
+		float lSSd = 3.820365E-13F;
+		short CelS = 11206;
+		float VQod = 3.900478E-18F;
+		short gNR = -26943;
+		short VGY = -31854;
+		byte cJuW = 217;
+		long long VLo = 17937783478868962;
+
+	}void Ifn() {
+		double SBcU = -511.3535;
+		byte pyxx = 159;
+		short MPLm = 19313;
+		float pbod = 5.922154E+33F;
+		double GwO = 1.398482E-15;
+		int kNRD = 344999895;
+		if (kNRD == 848002) {
+			kNRD = 795262;
+		}float quc = 8.094193E-35F;
+		long long Aohz = 69382709050996364;
+		float KiJ = 4.437145E+07F;
+		double ttny = 3.066704E+25;
+		double JuKS = -4.415064E+32;
+		int LXUI = 6057;
+		float hJg = -3.133486E-34F;
+		int zCaD = 59005046;
+		if (zCaD == 104003) {
+			zCaD = 618422;
+		}float zzty = -3.405786E+33F;
+		byte yoF = 156;
+		byte kOeP = 94;
+		byte djW = 102;
+		double JsF = -1.660521E+12;
+		double jSw = -4.493418E-26;
+		int TNW = 360423800;
+		if (TNW == 5799) {
+			TNW = 255628;
+		}
+	}void jCMS() {
+		byte dgh = 21;
+		int blgM = 93483976;
+		short HIA = -25160;
+		long long SEW = 64182783517824850;
+		float Wag = 4.284701E-16F;
+		int Wzt = 44933034;
+		long long XOXm = 69512772340389115;
+		double MJju = -4.797075E+11;
+		short pngE = -17893;
+		short cbkO = 9579;
+		int VJnz = 5476;
+		if (VJnz == 182181) {
+			VJnz = VJnz + 146285;
+		}byte zsB = 236;
+		long long NXqs = 13634554670880190;
+		double MyWb = 7.401471E+09;
+		float ucVR = 5.665233E-23F;
+		long long zZeG = 18303802167864636;
+		short dKR = -30792;
+		int Omg = 406643;
+		short hZXO = 30910;
+		int BKq = 76151809;
+		int GaO = 69;
+		if (GaO == 159903) {
+			GaO += 620191;
+		}int Kxb = 68203517;
+		if (Kxb == 221671) {
+			Kxb = 251326;
+		}float lFo = -17901.87F;
+		long long pJkX = 52646219925547467;
+		byte pDg = 27;
+		long long pVVB = 31385014321297474;
+		long long uNLu = 20432451061274344;
+
+	}void XMFu() {
+		int mCId = 9556;
+		if (mCId == 9556) {
+			mCId = 980362;
+		}int YuH = 252526;
+		if (YuH == 252526) {
+			YuH = YuH + 330696;
+		}long long JUoQ = 49518095664582868;
+		double VBd = 2.416159E-08;
+		int mgdi = 893669741;
+		if (mgdi == 470835) {
+			mgdi += 140523;
+		}int tQFC = 60;
+		if (tQFC == 671303) {
+			tQFC = tQFC + 97643;
+		}float hCS = -1.038782E-07F;
+		float PKxR = 1.294426E+36F;
+		float fIT = 2.349766E-05F;
+		short Wbuc = -898;
+		byte ZiYB = 243;
+		float AyT = -6.981185E+23F;
+		int HRaK = 97;
+		if (HRaK == 306990) {
+			HRaK += 835544;
+		}float pYoQ = -6.046542E-27F;
+		short Ufl = 15566;
+		long long Rgp = 43201752941107494;
+		float OEuA = 1.19914E-28F;
+		float Rnmi = 5.239664E+32F;
+		double zNd = 2447319;
+		float aeFB = -2.080735E-26F;
+		short zHWO = -20913;
+		int czT = 27;
+		if (czT == 556309) {
+			czT = czT + 937651;
+		}
+	}
